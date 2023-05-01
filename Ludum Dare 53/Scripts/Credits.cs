c2cfcb1fbcd9c805e795a54791db34bbb76c369c
Pixel Prophecy <phil@pixelprophecy.com>
2023-05-01 06:36:39 +0200

    Inserted & Styled Side bar, updated style of buttons

--- Ludum Dare 53/Scripts/Credits.cs
+++ Ludum Dare 53/Scripts/Credits.cs
@@ -2,14 +2,14 @@ using Godot;
 
 public partial class Credits : Node
 {
-    public void OnBackButtonPressed()
-    {   
-        GetTree().ChangeSceneToFile(@"res://Scenes/Menu.tscn");
-    }
+	public void OnBackButtonPressed()
+	{   
+		GetTree().ChangeSceneToFile(@"res://Scenes/Menu.tscn");
+	}
 
-    public override void _Process(double delta)
-    {
-        if (Input.IsActionJustPressed("ui_accept") || Input.IsActionJustPressed("ui_cancel"))
-            OnBackButtonPressed();
-    }
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustPressed("ui_accept") || Input.IsActionJustPressed("ui_cancel"))
+			OnBackButtonPressed();
+	}
 }
--- Ludum Dare 53/Scripts/GameOver.cs
+++ Ludum Dare 53/Scripts/GameOver.cs
@@ -2,22 +2,22 @@ using Godot;
 
 public partial class GameOver : Control
 {
-    public void OnRetryButtonPressed()
-    {
-        GetTree().ChangeSceneToFile(@"res://Scenes/Main.tscn");
-    }
+	public void OnRetryButtonPressed()
+	{
+		GetTree().ChangeSceneToFile(@"res://Scenes/Main.tscn");
+	}
 
-    public void OnQuitButtonPressed()
-    {
-        GetTree().Quit();
-    }
+	public void OnQuitButtonPressed()
+	{
+		GetTree().Quit();
+	}
 
-    public override void _Process(double delta)
-    {
-        if (Input.IsActionJustPressed("ui_accept"))
-            OnRetryButtonPressed();
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustPressed("ui_accept"))
+			OnRetryButtonPressed();
 
-        if (Input.IsActionJustPressed("ui_cancel"))
-            OnQuitButtonPressed();
-    }
+		if (Input.IsActionJustPressed("ui_cancel"))
+			OnQuitButtonPressed();
+	}
 }
